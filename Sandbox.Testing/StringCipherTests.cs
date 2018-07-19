200d9825c2c7e432b7c684474c603c248ccf4422
Steff Beckers <steff@steffbeckers.eu>
2018-07-19 15:35:12 +0200

    Initial commit StringCipher code added

---
+++ Sandbox.Testing/StringCipherTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sandbox.Testing
+{
+    [TestClass]
+    public class StringCipherTests
+    {
+        [TestMethod]
+        public void EncryptionTest()
+        {
+        }
+
+        [TestMethod]
+        public void DecryptionTest()
+        {
+        }
+    }
+}
