203132ffd2f750318d759328b414d424560bf912
Steff Beckers <beckerst@aarixa.be>
2019-01-03 13:01:08 +0100

    XML Tests added

---
+++ Sandbox.Testing/DateTimeTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sandbox.Testing
+{
+    [TestClass]
+    public class DateTimeTests
+    {
+        [TestMethod]
+        public void FormatDateTime1()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ Sandbox/Dossier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Export
+    {
+        public List<Dossier> Dossiers { get; set; }
+    }
+
+    public class Dossier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Extension { get; set; }
+    }
+}
